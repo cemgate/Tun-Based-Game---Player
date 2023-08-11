30328521edd8824706322c1334f1bdc71ea6357f
Marcin Cichocki <marcincgold@gmail.com>
2023-08-11 06:36:49 +0200

    doxygen

--- Player/Base.h
+++ Player/Base.h
@@ -1,36 +1,119 @@
 #pragma once
 #include "Entity.h"
 
+/**
+ * @class Base
+ * @brief Represents a base in the game.
+ *
+ * This class encapsulates attributes and behavior of a base in the game.
+ * It includes information about gold, health, position, build status, and more.
+ */
 class Base
 {
 public:
+    /**
+     * @brief Get the amount of gold the base has.
+     * @return The current gold amount.
+     */
+    long getGold();
 
-	long getGold();
-	int getHealth();
-	int getBaseUniqueID();
-	int getBasePosX();
-	int getBasePosY();
+    /**
+     * @brief Get the health of the base.
+     * @return The current health of the base.
+     */
+    int getHealth();
 
-	void setGold(long gold);
-	void setHealth(int health);
-	void setPosition(int x, int y);
-	void setBuildStatus(char entityType);
-	void setBaseUniqueID(int ID);
+    /**
+     * @brief Get the unique ID of the base.
+     * @return The unique ID of the base.
+     */
+    int getBaseUniqueID();
 
-	void takeDamage(int damage);
-	void entityCostOfPurchase(int cost);
-	bool isAlive();
-	bool isReadyToBuy(int cost);
-	bool isReadyToBuild();
-	~Base() {};
+    /**
+     * @brief Get the X position of the base.
+     * @return The X position of the base.
+     */
+    int getBasePosX();
+
+    /**
+     * @brief Get the Y position of the base.
+     * @return The Y position of the base.
+     */
+    int getBasePosY();
+
+    /**
+     * @brief Set the amount of gold for the base.
+     * @param gold The amount of gold to set.
+     */
+    void setGold(long gold);
+
+    /**
+     * @brief Set the health of the base.
+     * @param health The health value to set.
+     */
+    void setHealth(int health);
+
+    /**
+     * @brief Set the position of the base.
+     * @param x The X position to set.
+     * @param y The Y position to set.
+     */
+    void setPosition(int x, int y);
+
+    /**
+     * @brief Set the build status of the base.
+     * @param entityType The type of entity being built.
+     */
+    void setBuildStatus(char entityType);
+
+    /**
+     * @brief Set the unique ID of the base.
+     * @param ID The unique ID to set.
+     */
+    void setBaseUniqueID(int ID);
+
+    /**
+     * @brief Apply damage to the base.
+     * @param damage The amount of damage to apply.
+     */
+    void takeDamage(int damage);
+
+    /**
+     * @brief Deduct the cost of an entity from the base's gold.
+     * @param cost The cost of the entity to deduct.
+     */
+    void entityCostOfPurchase(int cost);
+
+    /**
+     * @brief Check if the base is still alive.
+     * @return True if the base's health is above 0, otherwise false.
+     */
+    bool isAlive();
+
+    /**
+     * @brief Check if the base is ready to buy an entity.
+     * @param cost The cost of the entity to check against.
+     * @return True if the base's gold is enough for the cost, otherwise false.
+     */
+    bool isReadyToBuy(int cost);
+
+    /**
+     * @brief Check if the base is ready to build an entity.
+     * @return True if the base is ready to build, otherwise false.
+     */
+    bool isReadyToBuild();
+
+    /**
+     * @brief Destructor for the Base class.
+     */
+    ~Base() {};
 
 private:
-
-	int baseUniqueID=-1;
-	long gold;
-	int health;	
-	Vector2f position;
-	bool isReadyToBuildEntity=true;
-	char buildingEntity;
+    int baseUniqueID = -1; ///< The unique ID of the base.
+    long gold; ///< The amount of gold the base has.
+    int health; ///< The health of the base.
+    Vector2f position; ///< The position of the base.
+    bool isReadyToBuildEntity = true; ///< Flag indicating if the base is ready to build an entity.
+    char buildingEntity; ///< The type of entity currently being built.
 };
 
--- Player/main.cpp
+++ Player/main.cpp
@@ -10,16 +10,10 @@ int main(int argc, char* argv[])
     
     if (argc >= 2) 
     {
-        Game game(argv[1], argv[2], argv[3], argv[4]);
-        std::string mapa = argv[1]; //string ze sciezka do pliku mapa.txt
-        std::string status = argv[2]; //string ze sciezka do pliku status.txt
-        std::string rozkazy = argv[3]; //string ze sciezka do pliku rozkazy.txt
-        std::string playerNumber = argv[4]; //string z informacja ktorym jestesmy graczem
+        Game game(argv[1], argv[2], argv[3], argv[4]); 
         try
         {
-            //std::cout << game.playerBase.getGold() << '\n';
-            game.mainAlgorithm();
-            //std::cout << game.playerBase.getGold() << '\n';
+            game.mainAlgorithm();  
         }
         catch (const std::exception& e)
         {
